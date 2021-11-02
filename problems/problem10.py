3f3f44e7bc86573cc632eda126169560a9252fae
Anna Giasson <AnnaGraceGiasson@gmail.com>
2021-11-01 22:29:41 -0400

    added basic research for problem 10

--- problems/problem10.py
+++ problems/problem10.py
@@ -7,3 +7,16 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
 """
+
+# More efficient prime number generators
+#
+# Sieves:
+# https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+# https://en.wikipedia.org/wiki/Sieve_of_Atkin
+#
+# Primality proving methods
+# https://en.wikipedia.org/wiki/AKS_primality_test#The_algorithm
+# https://en.wikipedia.org/wiki/Elliptic_curve_primality
+# https://en.wikipedia.org/wiki/Baillie%E2%80%93PSW_primality_test
+
+# Once a method is chosen pick an appropreiate data structure.
