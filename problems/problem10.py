f4e5bd02e2fe6cbe25bd0313ca1efec77b02e0b9
Anna Giasson <AnnaGraceGiasson@gmail.com>
2021-11-03 22:27:32 -0400

    solved problem 10 using sieve, confirmed answer

--- problems/problem10.py
+++ problems/problem10.py
@@ -11,7 +11,6 @@ Find the sum of all the primes below two million.
 # More efficient prime number generators
 #
 # Sieves:
-# https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 # https://en.wikipedia.org/wiki/Sieve_of_Atkin
 #
 # Primality proving methods
@@ -20,3 +19,34 @@ Find the sum of all the primes below two million.
 # https://en.wikipedia.org/wiki/Baillie%E2%80%93PSW_primality_test
 
 # Once a method is chosen pick an appropreiate data structure.
+
+from typing import List
+
+
+def sieve_eratosthenes(n_max: int) -> List[int]:
+    # https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+
+    primes: List[int] = []
+
+    if n_max < 2:
+        return primes
+
+    # list of int, flagged as prime by default
+    domain = [True]*n_max
+    domain[0:2] = (False, False)  # 0 and 1 are not prime
+
+    for n, is_prime in enumerate(domain):
+        if not is_prime:
+            continue
+        primes.append(n)
+
+        # flag multiples as not prime
+        for idx in range(n, n_max, n):
+            domain[idx] = False
+
+    return primes
+
+
+primes = sieve_eratosthenes(2_000_000)
+print(sum(primes))
+# ans: 142913828922
