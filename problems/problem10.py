9617466634b312dce5838b3654a485d3aae23329
Anna Giasson <AnnaGraceGiasson@gmail.com>
2021-11-04 19:15:36 -0400

    added description for problem 11

--- problems/problem10.py
+++ problems/problem10.py
@@ -14,7 +14,6 @@ Find the sum of all the primes below two million.
 # https://en.wikipedia.org/wiki/Sieve_of_Atkin
 #
 # Primality proving methods
-# https://en.wikipedia.org/wiki/AKS_primality_test#The_algorithm
 # https://en.wikipedia.org/wiki/Elliptic_curve_primality
 # https://en.wikipedia.org/wiki/Baillie%E2%80%93PSW_primality_test
 
